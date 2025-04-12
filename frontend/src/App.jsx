f9e223e24bc2268e40d77d79707647af2d186216
freniecki <247773@edu.p.lodz.pl>
2025-04-12 19:45:57 +0200

    security base view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,34 @@
-import './App.css'
+import "./App.css";
 import ChatWindow from "./Chat/ChatWindow.jsx";
-import {ChatHistory} from "./ChatHistory/ChatHistory.js";
+import { ChatHistory } from "./ChatHistory/ChatHistory.js";
 import Menu from "./Menu/Menu.jsx";
+import Auth from "./Auth/Auth.jsx";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
-        <Routes>
-            <Route path="/" element={<Menu />} />
-            <Route path="/ChatWindow" element={
-                <div className="content">
-                    <div className="chat-window" >
-                        <ChatWindow />
-                    </div >
-                    <div className="chat-history" >
-                        <ChatHistory />
-                    </div >
-                </div >
-            } />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route
+          path="/ChatWindow"
+          element={
+            <div className="content">
+              <div className="chat-window">
+                <ChatWindow />
+              </div>
+              <div className="chat-history">
+                <ChatHistory />
+              </div>
+            </div>
+          }
+        />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/oauth/callback/*" element={<Auth />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Auth/Auth.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect } from "react";
+
+const Auth = () => {
+  const handleOAuth = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8000/auth/google/authorize",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        },
+      );
+
+      if (response.status !== 200) {
+        throw new Error(
+          "Error while fetching google authorization request: ${response.status}",
+        );
+      }
+
+      const data = await response.json();
+      window.location.href = data.authorization_url;
+    } catch (err) {
+      console.error("Błąd: ", err);
+    }
+  };
+
+  const handleAuthTest = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "http://localhost:8000/authenticated-route",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        },
+      );
+
+      if (response.status !== 200) {
+        alert("shit happens");
+        throw new Error(
+          "Error while fetching google authorization request: ${response.status}",
+        );
+      }
+
+      const data = await response.json();
+      alert("Authenticated route response: " + JSON.stringify(data));
+    } catch (err) {
+      console.error("Błąd: ", err);
+    }
+  };
+
+  useEffect(() => {
+    if (window.location.pathname === "/oauth/callback") {
+      const query = window.location.search;
+      // alert("Received callback with query: " + query);
+
+      fetch("http://localhost:8000/auth/google/callback" + query, {
+        method: "GET",
+        credentials: "include",
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data.access_token) {
+            localStorage.setItem("token", data.access_token);
+            console.log("Token saved to local storage");
+            alert("Login successful" + data.access_token);
+
+            navigate("/");
+          } else {
+            console.log("Token not found in response");
+            alert("Login failed");
+          }
+        })
+        .catch(console.error);
+    }
+  });
+
+  return (
+    <div className="loginContainer">
+      <div className="loginWindow">
+        <div className="loginHeader">Login</div>
+        <div className="authorizationSelection">
+          <form className="oauthAuthorization" onSubmit={handleOAuth}>
+            <button type="submit">Google</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="authTestWindow">
+        {localStorage.getItem("token") && (
+          <div>
+            <div className="authTestHeader">Test</div>
+            <form className="authTestForm" onSubmit={handleAuthTest}>
+              <button type="submit">Test authorization</button>
+            </form>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
