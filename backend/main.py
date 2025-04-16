c0833c40a636785a084ff37d7e100cf89245dfc9
Yehor <ka.egor.ka@gmail.com>
2025-04-16 17:16:20 +0200

    fixed: fixed method to get answers from persona

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,34 @@
+from dotenv import load_dotenv
+from huggingface_hub import login
 import json
-import sys
 import os
+from pydantic import BaseModel
+from security.app import app
+import sys
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import uuid
-from datetime import datetime
+import uvicorn
 
-from starlette import status
+from personas import insert_persona
 
-from backend.personas import insert_persona
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../security'))
 
-import uvicorn
-from pydantic import BaseModel
-from security.app import app
-import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+load_dotenv()
 
 
-
-#TODO: REMOVE THIS SHIT(Tutaj musi być logowanie do huggingface)
-#TODO: REMOVE THIS SHIT
-
-
-
+# login to HF
+# you should place the HugginFace token in the .env
+# hf_token=<hf_token>
+hf_token = os.getenv('hf_token')
+login(token=hf_token)
 
 # app = FastAPI()
 
 # Load the base model name
 with open("../Neeko/data/train_output/adapter_config.json", "r") as f:
     adapter_config = json.load(f)
-
 base_model_path = adapter_config["base_model_name_or_path"]
 
 # Load the base model
@@ -40,8 +39,10 @@ tokenizer = AutoTokenizer.from_pretrained(base_model_path)
 adapter_weights = torch.load("../Neeko/data/train_output/adapter_model.bin")
 model.load_state_dict(adapter_weights, strict=False)
 
+
 class UserMessage(BaseModel):
-    message: str
+    prompt: str
+    persona: str
 
 
 class UserPersonaData(BaseModel):
@@ -50,37 +51,9 @@ class UserPersonaData(BaseModel):
     persona_description: str
 
 
-@app.get("/api/predefined_personas")
-async def all_predefined_personas():
-    """
-    Returns list of predefined personas
-    :return:
-    :rtype:
-    """
-    personas = [
-        {
-            'name': 'Persona1',
-            'id': '1',
-            'description': 'Persona 1 description'
-        },
-        {
-            'name': 'Persona2',
-            'id': '2',
-            'description': 'Persona 2 description'
-        },
-        {
-            'name': 'Persona3',
-            'id': '3',
-            'description': 'Persona 3 description'
-        }
-    ]
-    return personas
-
-
 @app.post('/api/add_persona')
 async def add_persona(request: UserPersonaData):
     insert_persona(request.user_id, request.persona_name, request.persona_description)
-
 
 
 @app.get('api/chat_history')
@@ -106,18 +79,20 @@ async def send_user_message(request: UserMessage):
         'response': 'I got your message'
     }
 
-@app.get('/api/get_answer')
-async def get_answer(prompt: str, persona_name: str):
+
+@app.post('/api/get_answer')
+async def get_answer(request: UserMessage):
     system_prompt = f"""
-        I want you to act like {persona_name}. I want you to respond and answer like {persona_name}, using the tone, manner and vocabulary {persona_name} would use. You must know all of the knowledge of {persona_name}.
+        I want you to act like {request.persona}. I want you to respond and answer like {request.persona},
+        using the tone, manner and vocabulary {request.persona} would use. 
+        You must know all of the knowledge of {request.persona}.
 
         The status of you is as follows:
         Location: Poland
-        Status: {status}
     
         The interactions are as follows:
         """
-    full_prompt = system_prompt + "\nUser: " + prompt + ":"
+    full_prompt = system_prompt + "\nUser: " + request.prompt + ":"
     # Tokenize input
     inputs = tokenizer(full_prompt, return_tensors="pt")
 
@@ -126,7 +101,7 @@ async def get_answer(prompt: str, persona_name: str):
         inputs["input_ids"],
         max_length=200,
         num_return_sequences=1,
-        temperature=0.7
+        temperature=0.6
     )
 
     # Decode the output
@@ -136,7 +111,7 @@ async def get_answer(prompt: str, persona_name: str):
     )
     return generated_text
 
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
     print(app.routes)
-
