e2085bd0d5096ea3f386151f3d95db3a461a4daa
Yehor <ka.egor.ka@gmail.com>
2025-04-16 19:11:09 +0200

    fixed the front for choosing persona

--- frontend/src/Menu/Menu.jsx
+++ frontend/src/Menu/Menu.jsx
@@ -45,51 +45,81 @@ function Menu() {
                   <button type="submit">
                         <img src="src/Menu/personas_photos/cleopatra.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Cleopatra
+                  </div>
               </form>
               <form action="/ChatWindow/Beethoven">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/beethoven.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Beethoven
+                  </div>
               </form>
               <form action="/ChatWindow/Caesar">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/caesar.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Caesar
+                  </div>
               </form>
               <form action="/ChatWindow/Hermione">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/hermione.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Hermione
+                  </div>
               </form>
               <form action="/ChatWindow/Newton">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/newton.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Newton
+                  </div>
               </form>
               <form action="/ChatWindow/MartinLutherKing">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/martin.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Martin L. K.
+                  </div>
               </form>
               <form action="/ChatWindow/Socrates">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/socrates.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Socrates
+                  </div>
               </form>
               <form action="/ChatWindow/Spartacus">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/spartacus.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Spartacus
+                  </div>
               </form>
               <form action="/ChatWindow/Voldemort">
                   <button type="submit">
                         <img src="src/Menu/personas_photos/voldemort.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Voldemort
+                  </div>
               </form>
               <form action={addPersona}>
                   <button type="submit">
                         <img src="src/Menu/personas_photos/plus.png" alt="Submit" width="100" height="50"/>
                   </button>
+                  <div className="persona-name">
+                      Add persona
+                  </div>
               </form>
               {isOpen && (
                   <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
