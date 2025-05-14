4ffc7133c2345767fcc3e521a22bc2d2dc880224
Yehor <ka.egor.ka@gmail.com>
2025-05-14 20:17:30 +0200

    setting up https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('./ssl/key.pem'),
+      cert: fs.readFileSync('./ssl/cert.pem'),
+    },
+    host: 'localhost',
+    port: 5137,
+  },
 })
