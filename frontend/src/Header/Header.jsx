461bb56f29263ef5fe512bb9a60391bdc63c25f3
249518 Aliaksei Vishniavetski <249518@edu.p.lodz.pl>
2025-05-19 19:09:17 +0200

    feat: switch header items places

--- frontend/src/Header/Header.jsx
+++ frontend/src/Header/Header.jsx
@@ -5,17 +5,17 @@ import AuthButton from '../Auth/AuthButton';
 function Header() {
   return (
     <header className="header">
+      <div className="main-header-text">
+          <Link to="/" className="main-header-text">
+            AI Personas
+          </Link>
+      </div>
       {<nav className="nav-bar">
         <Link to="/">
           <button className="nav-button">Home</button>
         </Link>
         <AuthButton />
       </nav>}
-    <div className="main-header-text">
-         <Link to="/" className="main-header-text">
-          AI Personas
-        </Link>
-    </div>
     </header>
   );
 }
